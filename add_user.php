5cb1bbbdf7befa298b868d1988733965610f7130
YIHONG <akashidaiki2@gmail.com>
2023-11-13 15:59:32 +0800

    完成登入登出功能

--- add_user.php
+++ add_user.php
@@ -1,9 +1,11 @@
 <?php
 $dsn = "mysql:host=localhost;charset=utf8;dbname=member";
 $pdo = new PDO($dsn, 'root', '');
+// 資料清洗 Data wash 的實際做法
+$acc = htmlspecialchars(trim($_POST['acc']));
 
 $sql = "insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
-                     values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['addr']}')";
+                     values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['addr']}')";
 
 $pdo->exec($sql);
 
