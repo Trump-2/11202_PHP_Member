adde07a055cee5f152aec403f3a696c02193e95e
YIHONG <akashidaiki2@gmail.com>
2023-11-17 09:38:27 +0800

    完成更新資料的動作並增加訊息回饋

--- copy/member.php
+++ copy/member.php
@@ -8,10 +8,8 @@ include_once "./include/connect.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>會員中心</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
   </script>
 </head>
 
@@ -27,26 +25,34 @@ include_once "./include/connect.php";
     </div>
     <div class="nav-item col-4">
       <?php
-            if (isset($_SESSION['user'])) {
-                echo "歡迎光臨 " . $_SESSION['user'];
-                echo "<a href='logout.php' class='btn btn-info mx-2'>登出</a>";
-                echo "<a href='member.php' class='btn btn-success mx-2'>會員中心</a>";
-            } else {
-            ?>
-      <a href="reg.php" class="btn btn-primary mx-2">註冊</a>
-      <a href="login_form.php" class="btn btn-success mx-2">登入</a>
+      if (isset($_SESSION['user'])) {
+        echo "歡迎光臨 " . $_SESSION['user'];
+        echo "<a href='logout.php' class='btn btn-info mx-2'>登出</a>";
+        echo "<a href='member.php' class='btn btn-success mx-2'>會員中心</a>";
+      } else {
+      ?>
+        <a href="reg.php" class="btn btn-primary mx-2">註冊</a>
+        <a href="login_form.php" class="btn btn-success mx-2">登入</a>
       <?php
-            }
-            ?>
+      }
+      ?>
 
     </div>
   </header>
   <div class="container">
     <h1>使用者資料</h1>
     <?php
-        $sql = "select * from users where `acc`='{$_SESSION['user']}'";
-        $user = $pdo->query($sql)->fetch();
-        ?>
+    if (isset($_SESSION['msg'])) {
+      echo "<div class='alert alert-warning text-center col-6 m-auto'>";
+      echo $_SESSION['msg'];
+      unset($_SESSION['msg']);
+      echo "</div>";
+    }
+
+    $sql = "select * from users where `acc`='{$_SESSION['user']}'";
+    $user = $pdo->query($sql)->fetch();
+
+    ?>
     <form action="update.php" method="post" class="col-4 m-auto">
       <div class="input-group my-1">
         <label class="col-4  input-group-text">帳號:</label>
@@ -69,6 +75,7 @@ include_once "./include/connect.php";
         <input class="form-control" type="text" name="address" id="address" value="<?= $user['address']; ?>">
       </div>
       <div>
+        <input type="hidden" name="id" value="<?= $user['id']; ?>">
         <input class="btn-primary mx-2" type="submit" value="更新">
         <input class="btn btn-warning mx-2" type="reset" value="重置">
         <input class="btn btn-danger mx-2" type="button" value="讓我消失吧">
